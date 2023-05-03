a1c59adb3c18057d8e9a108d018e340a60ba9fd0
CarolinaRaIs <115561193+CarolinaRaIs@users.noreply.github.com>
2023-05-02 19:12:50 -0700

    Defined routes that handle POST request to /api/users, /api/users/login, and /api/users/logout in the user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -29,3 +29,59 @@ router.post('/', async (req, res) => {
     }
 });
 
+// Define a route that handles a POST request to /api/users/login
+router.post('/login', async (req, res) => {
+    try {
+      // Find a user in the database with the email sent in the request body
+      const userData = await User.findOne({ where: { email: req.body.email } });
+  
+      // If no user was found, respond with an error message and a status code of 400
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+
+// Check if the password sent in the request matches the user's password
+const validPassword = await userData.checkPassword(req.body.password);    
+
+// If the passwords don't match, respond with an error message and a status code of 400
+if (!validPassword) {
+    res
+      .status(400)
+      .json({ message: 'Incorrect email or password, please try again' });
+    return;
+  }
+
+// Save the user's ID and logged-in status to the session
+req.session.save(() => {
+    req.session.user_id = userData.id;
+    req.session.logged_in = true;
+
+    // Respond with the user's data and a success message
+    res.json({ user: userData, message: 'You are now logged in!' });
+  });
+
+} catch (err) {
+  // If there was an error, respond with the error and a status code of 400
+  res.status(400).json(err);
+}
+});
+
+// Define a route that handles a POST request to /api/users/logout
+router.post('/logout', withAuth, (req, res) => {
+    // If the user is logged in
+    if (req.session.logged_in) {
+      // Destroy the session and respond with a status code of 204 (No Content)
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      // If the user is not logged in, respond with a status code of 404 (Not Found)
+      res.status(404).end();
+    }
+  });
+
+// Export the router so it can be used by the rest of our application
+module.exports = router;
