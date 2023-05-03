c3748eca8203d1441c7956ed150db940b1d2b05d
CarolinaRaIs <115561193+CarolinaRaIs@users.noreply.github.com>
2023-05-03 02:53:27 -0700

    included updating and deleting route in allergy-routes.js

--- controllers/api/allergy-routes.js
+++ controllers/api/allergy-routes.js
@@ -7,14 +7,39 @@ const withAuth = require('/utils/auth');
 // Route for adding a child's allergy
 router.post('/allergies', withAuth, async (req, res) => {
     try {
-      const { allergy_name, child_id } = req.body;
-      const newAllergy = await Allergy.create({ name: allergy_name });
-      const child = await Child.findByPk(child_id);
-      await child.addAllergy(newAllergy);
-      res.status(200).json(newAllergy);
+        const { allergy_name, child_id } = req.body;
+        const newAllergy = await Allergy.create({ name: allergy_name });
+        const child = await Child.findByPk(child_id);
+        await child.addAllergy(newAllergy);
+        res.status(200).json(newAllergy);
     } catch (err) {
-      res.status(500).json(err);
+        res.status(500).json(err);
     }
   });
   
+// Route for updating a child's allergy
+router.put('/allergies/:id', withAuth, async (req, res) => {
+    try {
+        const { allergy_name } = req.body;
+        const updatedAllergy = await Allergy.update(
+            { name: allergy_name },
+            { where: { id: req.params.id } }
+        );
+        res.status(200).json(updatedAllergy);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+  });
+  
+// Route for deleting a child's allergy
+router.delete('/allergies/:id', withAuth, async (req, res) => {
+    try {
+        const deletedAllergy = await Allergy.destroy({ where: { id: req.params.id } });
+        res.status(200).json(deletedAllergy);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
  
