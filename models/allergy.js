ccd0d60ce62e3cc487c3b653646410dcc1656a1d
Kevin Jessie <kevinjessie32@yahoo.com>
2023-05-02 20:35:20 -0700

    updated models

---
+++ models/allergy.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Allergy extends Model {}
+
+Allergy.init(
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    allergy_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'allergy'
+    },
+);
+
+module.exports = Allergy;
--- models/user.js
+++ models/user.js
@@ -8,3 +8,69 @@ class User extends Model {
     }
 }
 
+// define table columns and configuration
+
+User.init(
+    {
+        // TABLE COLUMN DEFINITIONS GO HERE
+        // define an id column
+        id: {
+            // use the special Sequelize DataTypes object provide what type of data it is
+            type: DataTypes.INTEGER,
+            // this is the equivalent of SQL's `NOT NULL` option
+            allowNull: false,
+            // instruct that this is the Primary Key
+            primaryKey: true,
+            // turn on auto increment
+            autoIncrement: true
+        },
+        // define a username column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        // define a email column
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+                }
+        },
+        // define a password column
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // this means the password must be at least four characters long
+                len: [8]
+            }
+        }
+    },
+    {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10)
+                return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality update password
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10)
+                return updatedUserData;
+            }
+
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
+   
+
