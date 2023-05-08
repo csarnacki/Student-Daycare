ad675e957f0cc49522ef539d277f671eec96c63d
Anthony-A-Perez <ap.erez@me.com>
2023-05-07 21:14:29 -0700

    got the logo image to render

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/img", express.static(path.join(__dirname, "public/img")));
 
 app.use(routes);
 
