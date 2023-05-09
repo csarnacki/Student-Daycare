7dc8670e324d5807742d265bd8e205818719ca53
CarolinaRaIs <115561193+CarolinaRaIs@users.noreply.github.com>
2023-05-08 17:09:17 -0700

    Made ports all cohesive in server.js, connection.js, and script.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
